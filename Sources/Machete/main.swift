e353b289280e6ea44fc964769ce2e8d0702f5244
Skip R <tinyslices@gmail.com>
2025-05-23 16:09:20 -0700

    move in-memory cache discovery to core

--- Sources/Machete/main.swift
+++ Sources/Machete/main.swift
@@ -1,31 +1,6 @@
-import CDyld
-import Foundation
 import MacheteCore
-import MachO
 
-var info = task_dyld_info_data_t()
-var count = mach_msg_type_number_t(
-  MemoryLayout<task_dyld_info_data_t>.size / MemoryLayout<natural_t>.size)
-guard
-  (withUnsafeMutablePointer(to: &info) {
-    $0.withMemoryRebound(to: integer_t.self, capacity: 1) {
-      task_info(mach_task_self_, task_flavor_t(TASK_DYLD_INFO), $0, &count)
-    }
-  }) == KERN_SUCCESS
-else {
-  fatalError("failed to grab task_info")
-}
-
-let allImageInfosPointer = UnsafePointer<dyld_all_image_infos>(
-  bitPattern: Int(info.all_image_info_addr))!
-let allImageInfos = allImageInfosPointer.pointee
-
-let cacheHeader = UnsafePointer<dyld_cache_header>(
-  bitPattern: allImageInfos.sharedCacheBaseAddress)!.pointee
-
-let sharedCache = SharedCache(unsafeLoadingFrom: UnsafeRawPointer(bitPattern: allImageInfos.sharedCacheBaseAddress)!, slide: allImageInfos.sharedCacheSlide)
-
-for image in sharedCache.images {
+for image in SharedCache.inMemory.images {
   print(image)
   for lc in image.loadCommands {
     print("  \(lc)")
--- Sources/MacheteCore/SharedCache.swift
+++ Sources/MacheteCore/SharedCache.swift
@@ -16,6 +16,32 @@ public extension SharedCache {
     guts = base.bindMemory(to: dyld_cache_header.self, capacity: 1)
     self.slide = slide
   }
+
+  static var inMemory: Self {
+    // `task_info(MACH_TASK_SELF, TASK_DYLD_INFO, &info, &count)`
+    let dyldInfo = {
+      var info = task_dyld_info_data_t()
+      var count = mach_msg_type_number_t(
+        MemoryLayout<task_dyld_info_data_t>.size / MemoryLayout<natural_t>.size)
+      guard
+        (withUnsafeMutablePointer(to: &info) {
+          $0.withMemoryRebound(to: integer_t.self, capacity: 1) {
+            task_info(mach_task_self_, task_flavor_t(TASK_DYLD_INFO), $0, &count)
+          }
+        }) == KERN_SUCCESS
+      else {
+        fatalError("couldn't grab task_info of the current task")
+      }
+      return info
+    }()
+
+    let allImageInfos = UnsafePointer<dyld_all_image_infos>(
+      bitPattern: Int(dyldInfo.all_image_info_addr))!.pointee
+    let sharedCacheBase = UnsafeRawPointer(bitPattern: allImageInfos.sharedCacheBaseAddress)!
+
+    return SharedCache(unsafeLoadingFrom: sharedCacheBase,
+                       slide: allImageInfos.sharedCacheSlide)
+  }
 }
 
 public extension SharedCache {

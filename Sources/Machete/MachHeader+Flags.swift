f414734270abfeb305757a7828f5d4767d8da071
Skip R <tinyslices@gmail.com>
2025-05-23 13:40:40 -0700

    mach: use imported constants for header flags

--- Sources/Machete/MachHeader+Flags.swift
+++ Sources/Machete/MachHeader+Flags.swift
@@ -1,52 +1,55 @@
 import Foundation
+import MachO
 public import OrderedCollections
 
 // TODO(skip): This is gross and ought to use macros, but swift-syntax compile
 // times are miserable.
 
-public extension MachHeader {
+extension MachHeader {
   // https://github.com/apple-oss-distributions/xnu/blob/e3723e1f17661b24996789d8afc084c0c3303b26/EXTERNAL_HEADERS/mach-o/loader.h#L125
-  struct Flags: RawRepresentable, OptionSet, Hashable, Sendable {
+  public struct Flags: RawRepresentable, OptionSet, Hashable, Sendable {
     public let rawValue: UInt32
 
     public init(rawValue: UInt32) {
       self.rawValue = rawValue
     }
 
-    public static let noUndefinedReferences = Self(rawValue: 1 << 0)
-    public static let incrementallyLinked = Self(rawValue: 1 << 1)
-    public static let dynamicallyLinked = Self(rawValue: 1 << 2)
-    public static let bindingUndefinedReferencesAtLoad = Self(rawValue: 1 << 3)
-    public static let prebound = Self(rawValue: 1 << 4)
-    public static let splitSegments = Self(rawValue: 1 << 5)
-    public static let sharedLibraryLazyInitialization = Self(rawValue: 1 << 6)
-    public static let twoLevelNamespaceBindings = Self(rawValue: 1 << 7)
-    public static let forcingFlatNameSpaceBindings = Self(rawValue: 1 << 8)
-    public static let noMultipleDefinitionsPerSymbol = Self(rawValue: 1 << 9)
-    public static let withoutFixingPrebinding = Self(rawValue: 1 << 10)
-    public static let prebindable = Self(rawValue: 1 << 11)
-    public static let allTwoLevelModulesBound = Self(rawValue: 1 << 12)
-    public static let sectionsDividableViaSymbols = Self(rawValue: 1 << 13)
-    public static let canonical = Self(rawValue: 1 << 14)
-    public static let containsExternalWeakSymbols = Self(rawValue: 1 << 15)
-    public static let usesWeakSymbols = Self(rawValue: 1 << 16)
-    public static let allowStackExecution = Self(rawValue: 1 << 17)
-    public static let rootSafe = Self(rawValue: 1 << 18)
-    public static let setUidSafe = Self(rawValue: 1 << 19)
-    public static let noReexportedDylibs = Self(rawValue: 1 << 20)
-    public static let pie = Self(rawValue: 1 << 21)
-    public static let deadStrippableDylib = Self(rawValue: 1 << 22)
-    public static let hasTLVDescriptors = Self(rawValue: 1 << 23)
-    public static let noHeapExecution = Self(rawValue: 1 << 24)
-    public static let appExtensionSafe = Self(rawValue: 1 << 25)
-    public static let nListOutOfSyncWithDyldInfo = Self(rawValue: 1 << 26)
-    public static let simulatorSupported = Self(rawValue: 1 << 27)
-    public static let dylibInCache = Self(rawValue: 1 << 31)
+    public static let noUndefinedReferences = Self(rawValue: UInt32(MH_NOUNDEFS))
+    public static let incrementallyLinked = Self(rawValue: UInt32(MH_INCRLINK))
+    public static let dynamicallyLinked = Self(rawValue: UInt32(MH_DYLDLINK))
+    public static let bindingUndefinedReferencesAtLoad = Self(rawValue: UInt32(MH_BINDATLOAD))
+    public static let prebound = Self(rawValue: UInt32(MH_PREBOUND))
+    public static let splitSegments = Self(rawValue: UInt32(MH_SPLIT_SEGS))
+    public static let sharedLibraryLazyInitialization = Self(rawValue: UInt32(MH_LAZY_INIT))
+    public static let twoLevelNamespaceBindings = Self(rawValue: UInt32(MH_TWOLEVEL))
+    public static let forcingFlatNameSpaceBindings = Self(rawValue: UInt32(MH_FORCE_FLAT))
+    public static let noMultipleDefinitionsPerSymbol = Self(rawValue: UInt32(MH_NOMULTIDEFS))
+    public static let withoutFixingPrebinding = Self(rawValue: UInt32(MH_NOFIXPREBINDING))
+    public static let prebindable = Self(rawValue: UInt32(MH_PREBINDABLE))
+    public static let allTwoLevelModulesBound = Self(rawValue: UInt32(MH_ALLMODSBOUND))
+    // swift-format-ignore
+    public static let sectionsDividableViaSymbols = Self(rawValue: UInt32(MH_SUBSECTIONS_VIA_SYMBOLS))
+    public static let canonical = Self(rawValue: UInt32(MH_CANONICAL))
+    public static let containsExternalWeakSymbols = Self(rawValue: UInt32(MH_WEAK_DEFINES))
+    public static let usesWeakSymbols = Self(rawValue: UInt32(MH_BINDS_TO_WEAK))
+    public static let allowStackExecution = Self(rawValue: UInt32(MH_ALLOW_STACK_EXECUTION))
+    public static let rootSafe = Self(rawValue: UInt32(MH_ROOT_SAFE))
+    public static let setUidSafe = Self(rawValue: UInt32(MH_SETUID_SAFE))
+    public static let noReexportedDylibs = Self(rawValue: UInt32(MH_NO_REEXPORTED_DYLIBS))
+    public static let pie = Self(rawValue: UInt32(MH_PIE))
+    public static let deadStrippableDylib = Self(rawValue: UInt32(MH_DEAD_STRIPPABLE_DYLIB))
+    public static let hasTLVDescriptors = Self(rawValue: UInt32(MH_HAS_TLV_DESCRIPTORS))
+    public static let noHeapExecution = Self(rawValue: UInt32(MH_NO_HEAP_EXECUTION))
+    public static let appExtensionSafe = Self(rawValue: UInt32(MH_APP_EXTENSION_SAFE))
+    // swift-format-ignore
+    public static let nListOutOfSyncWithDyldInfo = Self(rawValue: UInt32(MH_NLIST_OUTOFSYNC_WITH_DYLDINFO))
+    public static let simulatorSupported = Self(rawValue: UInt32(MH_SIM_SUPPORT))
+    public static let dylibInCache = Self(rawValue: UInt32(MH_DYLIB_IN_CACHE))
   }
 }
 
-public extension MachHeader.Flags {
-  static let allFlagsDescribed: OrderedDictionary<Self, String> = [
+extension MachHeader.Flags {
+  public static let allFlagsDescribed: OrderedDictionary<Self, String> = [
     .noUndefinedReferences: "MH_NOUNDEFS",
     .incrementallyLinked: "MH_INCRLINK",
     .dynamicallyLinked: "MH_DYLDLINK",

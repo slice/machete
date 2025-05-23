bafe218d0caa0bbb488c4724570c72753334ff76
Skip R <tinyslices@gmail.com>
2025-05-22 23:40:04 -0700

    iterate over all images in the cache

--- Machete/main.swift
+++ Machete/main.swift
@@ -11,8 +11,22 @@ guard (withUnsafeMutablePointer(to: &info) {
   fatalError("failed to grab task_info")
 }
 
-dump(info)
+let allImageInfosPointer = UnsafePointer<dyld_all_image_infos>(bitPattern: Int(info.all_image_info_addr))!
+let allImageInfos = allImageInfosPointer.pointee
 
-let infoPointer = UnsafePointer<dyld_all_image_infos>(bitPattern: Int(info.all_image_info_addr))!
+let cacheHeader = UnsafePointer<dyld_cache_header>(bitPattern: allImageInfos.sharedCacheBaseAddress)!.pointee
 
-dump(infoPointer.pointee)
+let magic = withUnsafeBytes(of: cacheHeader.magic) {
+  $0.withMemoryRebound(to: UInt8.self) { String(cString: $0.baseAddress!) }
+}
+print(magic, "(\(cacheHeader.imagesCount) images)")
+let images = UnsafeBufferPointer<dyld_cache_image_info>(
+  start: UnsafePointer(bitPattern: allImageInfos.sharedCacheBaseAddress + UInt(cacheHeader.imagesOffset)),
+  count: Int(cacheHeader.imagesCount)
+)
+
+for (imageIndex, image) in images.enumerated() {
+  let path = String(cString: UnsafePointer<CChar>(bitPattern: allImageInfos.sharedCacheBaseAddress + UInt(image.pathFileOffset))!)
+  let paddedImageIndex = String(format: "% 8d", imageIndex)
+  print("\(paddedImageIndex)", path)
+}
